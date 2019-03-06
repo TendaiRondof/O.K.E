8097aefb3d3991e1c2bfc43a9cf29bb9935d4906
TendaiRondof <tendai.rondof@stud.msw.ch>
2019-03-06 13:50:37 +0100

    New Trained Yolo

--- KI/Data_set/coco_qqwwee.py
+++ KI/Data_set/coco_qqwwee.py
@@ -3,6 +3,8 @@ import os
 import requests
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt 
+import re
 
 filename = "Data_set/coco_data.json"
 
@@ -11,17 +13,33 @@ with open(filename, 'r') as f:
     all_images = full_json_file["images"]
     all_anotations = full_json_file["annotations"]
 
-    data_array = {}
+    data_dic = {}
 
     for image in all_images:
         img_as_bytes = requests.get(image["file_name"], stream=True).content
+        with open("Data_set/Images/"+image["id"]+".jpg", 'wb') as handler:
+            handler.write(img_as_bytes)
 
-        img = np.frombuffer(img_as_bytes,dtype=np.uint8)
-        print(img[:10])
-        img = img.reshape((image["height"],image["width"],3))
-        cv2.imshow("bla",img)
-        data_array[image["id"]] = [img]
+        img_path = "Data_set/Images/"+image["id"]+".jpg"
+        data_dic[image["id"]] = [img_path]
 
     for annotation in all_anotations:
-        data_array[annotation["image_id"]].append(annotation["bbox"])
+        data_dic[annotation["image_id"]].append(annotation["bbox"])
+
+    with open("train.txt","w") as file:
+        string = ""
+        for data in data_dic:
+            for element in data_dic[data]:
+                if isinstance(element[0],float) :
+                    save_array = [int(val) for val in element]+[1]
+                else:
+                    save_array = element
+                string+= re.sub(r"[\[\] ]",'',str(save_array))+' '
+            string+="\n"
+        
+        file.write(string)
+        
+
+
+    
 
